74f42366f52f8aedf198663629ce6bdfef4f7adf
Kevin Meireles <105472882+Kebions2077@users.noreply.github.com>
2025-05-10 22:38:25 -0300

    versão final

--- Prova 1/Ex3.c
+++ Prova 1/Ex3.c
@@ -1,60 +1,59 @@
 #include <stdio.h>
-    void preenche(int vet[10]);
-    int cal_maior (int vet[10]);
-    int cal_soma( int vet[10]);
-    float cal_media( int soma);
 
-        void preenche(int vet[10]){
-            int x;
-            x=0;
-            for(x=0;x<10;x++){
-                printf("Digite a posição [%i]", x);
-                    scanf("%i", &vet[x]);
-            }
-        }
-        int cal_soma(int soma,int vet[10]){
-            soma= vet[0]+vet[1]+vet[2]+vet[3]+vet[4]+vet[5]+vet[6]+vet[7]+vet[8]+vet[9];
-            return soma;
-        }
-        float cal_media( int soma){
-             
-                return soma / 10; 
-        }
-        int cal_maior(int maior, int vet[10]){
-            int x;
-            x=0;
-            maior=vet[0];
-                for(x=0;x<10;x++){
-                    if (vet[x]>maior){
-                        maior=vet[x];
-                    }
-                }
-                return maior;
-        }
+void preenche(int vet[10]);
+int cal_maior(int vet[10]);
+int cal_soma(int vet[10]);
+float cal_media(int soma);
 
-        int main(void){
-            int x,vet[10], maior, soma;
-            float media;
-            x=0;
-            
-            vet= preenche(vet);
-            
-            printf("O vetor de 10 posições:");
-            
-            for(x=0;x<10;x++){
-                printf("[%i],vet[x]");
-            }
-            
-            cal_soma=soma(vet);
-            
-            printf("A soma de todos os valores do vetor %i", soma);
-            
-            cal_media= cal_media(soma);
-            
-            printf("A media Dos elementos do vetor:%.2f", media);
+void preenche(int vet[10]) {
+    for(int x = 0; x < 10; x++) {
+        printf("Digite a posição [%d]: ", x);
+        scanf("%d", &vet[x]);
+    }
+}
 
-            cal_maior= maior(vet);
+int cal_soma(int vet[10]) {
+    int soma = 0;
+    for(int x = 0; x < 10; x++) {
+        soma += vet[x];
+    }
+    return soma;
+}
 
-            printf("O maior numero armazenado no vetor é: %i", maior);
-            
+float cal_media(int soma) {
+    return (float)soma / 10;  // Conversão para float para divisão exata
+}
+
+int cal_maior(int vet[10]) {
+    int maior = vet[0];
+    for(int x = 1; x < 10; x++) {
+        if(vet[x] > maior) {
+            maior = vet[x];
         }
+    }
+    return maior;
+}
+
+int main() {
+    int vet[10], maior, soma;
+    float media;
+    
+    preenche(vet);
+    
+    printf("Vetor de 10 posições:\n");
+    for(int x = 0; x < 10; x++) {
+        printf("[%d] ", vet[x]);
+    }
+    printf("\n");
+    
+    soma = cal_soma(vet);
+    printf("Soma dos valores: %d\n", soma);
+    
+    media = cal_media(soma);
+    printf("Média dos elementos: %.2f\n", media);
+    
+    maior = cal_maior(vet);
+    printf("Maior valor no vetor: %d\n", maior);
+    
+    return 0;
+}
