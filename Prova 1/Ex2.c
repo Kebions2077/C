3c5db6eeef34d696cec4d1a55959839df1ce1b90
Kevin Meireles <105472882+Kebions2077@users.noreply.github.com>
2025-05-11 21:59:35 -0300

    Update Ex2.c

--- Prova 1/Ex2.c
+++ Prova 1/Ex2.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
-#include <math.h>
-   int cal_cubo(int num);
-      int cal_cubo(int num){
-        int exp;
-        exp=3;
-        
-        return pow (num,exp);
-      }  
-      int main(void){
-        int num,cubo;
-        printf("Digite um numero para calcular o cubo:\n");
-            scanf("%i", &num);
-            cubo= cal_cubo(num);
-            printf("O Cubo do numero %i: %i", num,cubo);
-            return 0;
+#include <math.h>  // Para a constante M_PI
 
-      }
+int main() {
+    printf("Valor de π: %.4f\n", M_PI*4);
+    return 0;
+}
